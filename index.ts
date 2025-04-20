0103c98086764bf7754714be09d5c52abeaf2cea
NEKO <account@naikho.com>
2025-04-20 12:17:07 +0200

    fix index.ts, rename ClassBase to VivaBases

--- index.ts
+++ index.ts
@@ -5,16 +5,16 @@
  * License: ISC
  */
 
+import { Marketplace } from './src/MarketPlace.class';
 import VivaAuth from './src/vivabases/VivaAuth.class';
 import VivaEndpoints from './src/vivabases/VivaEndpoints.class';
 import VivaSkull from './src/vivabases/VivaSkull.class';
 import Vivawallet, { getSmartCheckout } from './src/Vivawallet.class';
-import { Marketplace } from './src/marketplace/MarketPlace.class';
 
-const ClassBase = {./src/MarketPlace.class
+const VivaBases = {
   VivaEndpoints,
   VivaSkull,
   VivaAuth,
 };
 
-export { Vivawallet, getSmartCheckout, Marketplace, ClassBase };
+export { Vivawallet, getSmartCheckout, Marketplace, VivaBases };
