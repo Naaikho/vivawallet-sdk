eab1b06e9d541cdba0ccd61f25246178af17503a
naaikho <account@naikho.com>
2026-01-21 20:17:05 +0100

    add new ISV classes ton ISV main class

--- src/VivaISV.class.ts
+++ src/VivaISV.class.ts
@@ -1,15 +1,21 @@
+import IsvConnectedAccounts from './isv/IsvConnectedAccounts.class';
 import IsvPayments from './isv/IsvPayments.class';
 import IsvPos from './isv/IsvPos.class';
+import IsvWebhook from './isv/IsvWebhook.class';
 import { VivawalletISVInit } from './types/Vivawallet.types';
 import { VivaAuthISV } from './vivabases/VivaAuth.class';
 
 export class VivaISV extends VivaAuthISV {
-  pos: IsvPos;
+  connectedAccounts: IsvConnectedAccounts;
   payments: IsvPayments;
+  pos: IsvPos;
+  webhook: IsvWebhook;
 
   constructor(datas: VivawalletISVInit) {
     super(datas);
-    this.pos = new IsvPos(datas);
+    this.connectedAccounts = new IsvConnectedAccounts(datas);
     this.payments = new IsvPayments(datas);
+    this.pos = new IsvPos(datas);
+    this.webhook = new IsvWebhook(datas);
   }
 }
